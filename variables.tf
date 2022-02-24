426c600969c3ad63aef5af495ae935a6cbe7b861
Leo Przybylski <leo@cloudposse.com>
2022-02-24 14:14:50 -0700

    Migrating component changes to module

--- variables.tf
+++ variables.tf
@@ -1,4 +1,30 @@
-variable "example" {
-  description = "Example variable"
-  default     = "hello world"
+variable "database_instance" {
+  description = "RDS database instance identifier used to derive connection information from SSM."
+  type        = string
+}
+
+variable "database_name" {
+  description = "Name of the database to create a connection to."
+  type        = string
+}
+
+variable "connection_type" {
+  description = "(Optional) The type of the connection. Supported are: JDBC, MONGODB, KAFKA, and NETWORK. Defaults to JBDC"
+  type        = string
+  default     = "JDBC"
+}
+
+variable "jdbc_database_type" {
+  description = "Database engine or type of database used. Supported database types are mysql, postgresql, oracle, and redshift."
+  type        = string
+}
+
+variable "region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "vpc_id" {
+  description = "Id of the VPC the database instance resides in."
+  type        = string
 }
